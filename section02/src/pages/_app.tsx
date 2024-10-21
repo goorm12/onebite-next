c06a729b6fca539196502086310bf7b3ed1a5ea8
binwoonki <vinwoonki98@gmail.com>
2024-10-21 21:49:57 +0900

    [prefetching] - Link 컴포넌트로 명시된 경로가 아니라면 프리페칭 x useEffect를 사용하여 router.prefetch 코드를 작성하면 된다.

--- section02/src/pages/_app.tsx
+++ section02/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Link from "next/link";
 import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
@@ -9,6 +10,10 @@ export default function App({ Component, pageProps }: AppProps) {
   const onClickButton = () => {
     router.push("/test");
   };
+
+  useEffect(() => {
+    router.prefetch("/test");
+  }, []);
 
   return (
     <>
