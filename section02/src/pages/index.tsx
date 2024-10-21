4de7cd968699f81b64927ce682da69ff52eeb831
binwoonki <vinwoonki98@gmail.com>
2024-10-21 21:07:22 +0900

    [삭제] - 폴더 삭제 및 기본 style 제거

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -1,14 +1,7 @@
-import Head from "next/head";
-import Image from "next/image";
-import { Inter } from "next/font/google";
-import styles from "@/styles/Home.module.css";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
   return (
     <>
-      <h1>안녕 NEXT.JS</h1>
+      <h1>인덱스</h1>
     </>
   );
 }
