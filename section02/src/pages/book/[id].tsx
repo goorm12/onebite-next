2a29f9de70e3d8e34f0513c0027ab82b99ed8a60
binwoonki <vinwoonki98@gmail.com>
2024-10-21 21:18:08 +0900

    [id.tsx] - useRouter를 이용하여 id값을 받아오기

--- section02/src/pages/book/[id].tsx
+++ section02/src/pages/book/[id].tsx
@@ -1,3 +1,8 @@
+import { useRouter } from "next/router";
+
 export default function Page() {
-  return <h1>Book</h1>;
+  const router = useRouter();
+  const { id } = router.query;
+
+  return <h1>Book {id}</h1>;
 }
