a0c08c94c2ddbe8f028ad06169689c2cf493849b
binwoonki <vinwoonki98@gmail.com>
2024-10-23 13:16:24 +0900

    [SSG] - 정적 사이트 생성

--- section02/src/pages/index.tsx
+++ section02/src/pages/index.tsx
@@ -2,21 +2,51 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
 import { ReactNode } from "react";
-import books from "@/mock/books.json";
-import BookItem from "@/components/book-item";
 
-export default function Home() {
+import BookItem from "@/components/book-item";
+import { InferGetServerSidePropsType, InferGetStaticPropsType } from "next";
+import fetchBooks from "@/\blib/fetch-books";
+import fetchRandomBooks from "@/\blib/fetch-random-books";
+
+// SSR 방식으로 사전 렌더링이 됨, 약속된 이름 getServerSideProps
+// export const getServerSideProps = async () => {
+//   const [allBooks, recoBooks] = await Promise.all([
+//     fetchBooks(),
+//     fetchRandomBooks(),
+//   ]);
+//   return {
+//     props: { allBooks, recoBooks },
+//   };
+// };
+
+// SSG 정적 페이지
+export const getStaticProps = async () => {
+  console.log("인덱스 페이지");
+
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
+  return {
+    props: { allBooks, recoBooks },
+  };
+};
+
+export default function Home({
+  allBooks,
+  recoBooks,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
         <h3>지금 추천하는 도서</h3>
-        {books.map((book) => (
+        {recoBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
       <section>
         <h3>등록된 모든 도서</h3>
-        {books.map((book) => (
+        {allBooks.map((book) => (
           <BookItem key={book.id} {...book} />
         ))}
       </section>
