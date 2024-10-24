8def2986cebb3da1359e89190f0ed57ebabe5792
binwoonki <vinwoonki98@gmail.com>
2024-10-24 23:00:45 +0900

    [서버 액션] - 클라이언트 컴포넌트에서의 서버 액션 #22

--- app-router/src/actions/create-review.action.ts
+++ app-router/src/actions/create-review.action.ts
@@ -2,12 +2,14 @@
 
 import { revalidateTag } from "next/cache";
 
-export async function createReviewAction(formData: FormData) {
+export async function createReviewAction(_: unknown, formData: FormData) {
   const bookId = formData.get("bookId")?.toString();
   const content = formData.get("content")?.toString();
   const author = formData.get("author")?.toString();
 
-  if (!bookId || !content || !author) return;
+  if (!bookId || !content || !author) {
+    return { status: false, error: "리뷰 내용과 작성자를 입력해주세요" };
+  }
 
   try {
     const response = await fetch(
@@ -17,10 +19,19 @@ export async function createReviewAction(formData: FormData) {
         body: JSON.stringify({ bookId, content, author }),
       }
     );
-    console.log(response.status);
+    if (!response.ok) {
+      throw new Error(response.statusText);
+    }
     revalidateTag(`review-${bookId}`);
+
+    return {
+      status: true,
+      error: "",
+    };
   } catch (error) {
-    console.error(error);
-    return;
+    return {
+      status: false,
+      error: `리뷰 저장에 실패했습니다 : ${error}`,
+    };
   }
 }
